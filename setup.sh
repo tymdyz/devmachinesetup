df3396dbaa9c5d3dcc89217710055037fce87b2b
tymdyz <122420545+tymdyz@users.noreply.github.com>
2025-06-22 20:16:04 +1000

    Update setup.sh
    Added packages to list to install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,25 @@
 #Script for setting up ubuntu as a dev machine by installing applications and dotfiles
 
-#install Neovim
+#App installs
+apt update
+
+#Install basic gui necessities
+apt install -y \
+    xserver-xorg-core \
+    xserver-xorg-input-all \
+    xserver-xorg-video-all \
+    xinit \
+    i3 \
+    lightdm \
+    lightdm-gtk-greeter \
+    dbus-x11 \
+    fonts-dejavu-core \
+    alsa-utils \
+    pulseaudio
+
+#Install key tools
+apt install -y \
+    neovim \
+    tmux \
+    firefox \
+    git
